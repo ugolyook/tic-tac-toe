9164759b67389ae4696d3585126993daeb9c6adb
sveta <sviatlana.graskova@gmail.com>
2026-01-15 20:45:23 +0300

    Corrected class boarder.java

---
+++ src/Boarder.java
@@ -0,0 +1,27 @@
+public class Boarder
+{
+    private char[][] matrix;
+
+    public Boarder() {
+        matrix = new char[3][3];
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = '*';
+            }
+        }
+    }
+
+    public void showInfo() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
